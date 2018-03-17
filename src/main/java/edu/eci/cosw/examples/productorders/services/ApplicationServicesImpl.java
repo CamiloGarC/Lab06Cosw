180c47411309c8a6514a2bfcfd00b02486c80db9
CamiloGarC <camilo.garcia-c@mail.escuelaing.edu.co>
2018-03-17 00:48:44 -0500

    Finish lab

---
+++ src/main/java/edu/eci/cosw/examples/productorders/services/ApplicationServicesImpl.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2016 Pivotal Software, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.eci.cosw.examples.productorders.services;
+
+import edu.eci.cosw.examples.productorders.repositories.ClientsRepository;
+import edu.eci.cosw.examples.productorders.repositories.DispatchRepository;
+import edu.eci.cosw.examples.productorders.repositories.OrdersRepository;
+import edu.eci.cosw.examples.productorders.repositories.ProductsRepository;
+import edu.eci.cosw.examples.productorders.repositories.VehiclesRepository;
+import edu.eci.cosw.samples.model.Cliente;
+import edu.eci.cosw.samples.model.Despacho;
+import edu.eci.cosw.samples.model.Pedido;
+import edu.eci.cosw.samples.model.Producto;
+import edu.eci.cosw.samples.model.Vehiculo;
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hcadavid
+ */
+@Service
+public class ApplicationServicesImpl implements ApplicationServices{
+   
+    @Autowired
+    private OrdersRepository ordrepo;
+
+    @Autowired
+    private ProductsRepository prorepo;
+    
+    @Autowired
+    private DispatchRepository disprepo;
+    
+    @Autowired
+    private ClientsRepository clirepo;
+
+    @Autowired
+    private VehiclesRepository vehirepo;
+    
+    @Override
+    public List<Pedido> getAllOrders() throws ServicesException{
+        List<Pedido> p=ordrepo.findAll();
+        return p;
+    }
+
+    @Override
+    public List<Producto> getAllProducts() throws ServicesException{
+        return  prorepo.findAll();
+    }
+
+    @Override
+    public Pedido orderById(Integer id) throws ServicesException{
+        return ordrepo.findOne(id);
+    }
+    
+    
+    @Override
+    public Despacho dispatchByID(Integer id) throws ServicesException{
+        return disprepo.findOne(id);
+    }
+    
+    @Override
+    public List<Vehiculo> getVehicles(int idProduct) throws ServicesException {
+        return vehirepo.getVehicles(idProduct);
+    }
+
+    @Override
+    public List<Cliente> getClients(long value) throws ServicesException {
+        return clirepo.getClientes(value);
+    }
+
+    @Override
+    public void addDispatch(Despacho dispatch) throws ServicesException {
+        disprepo.saveAndFlush(dispatch);
+    }
+    
+    @Override
+    public Vehiculo vehicleById(String id) throws ServicesException {
+        return vehirepo.findOne(id);
+    }
+    
+}
